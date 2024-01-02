47c4cfefe3129cccf136754b261fb7e7018097ff
VK <24637852+krynv@users.noreply.github.com>
2024-01-02 13:31:37 +0000

    Created the 'manage members' modal

---
+++ components/modals/members-modal.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+import { useModal } from "@/hooks/use-modal-store";
+import { ServerWithMembersWithProfiles } from "@/types";
+
+
+export const MembersModal = () => {
+  const { onOpen, isOpen, onClose, type, data } = useModal();
+
+  const isModalOpen = isOpen && type === "members";
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { server } = data as { server: ServerWithMembersWithProfiles };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Manage Members
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            {server?.members?.length} {server?.members?.length === 1 ? 'Member' : 'Members'}
+          </DialogDescription>
+        </DialogHeader>
+        <ScrollArea className="mt-8 max-h-[420px] pr-6">
+          {server?.members?.map((member) => (
+            <div key={member.id} className="flex items-center gap-x-2 mb-6">
+              Text
+            </div>
+          ))}
+        </ScrollArea>
+
+
+      </DialogContent>
+    </Dialog>
+  );
+}
+
