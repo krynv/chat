7851f5faad0a40f7002e47708316792bca82a536
VK <24637852+krynv@users.noreply.github.com>
2024-01-02 10:20:29 +0000

    Added invite modal

---
+++ components/modals/invite-modal.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useModal } from "@/hooks/use-modal-store";
+
+export const InviteModal = () => {
+  const { isOpen, onClose, type } = useModal();
+
+  const isModalOpen = isOpen && type === "invite";
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Invite
+          </DialogTitle>
+        </DialogHeader>
+
+      </DialogContent>
+    </Dialog>
+  );
+}
+
