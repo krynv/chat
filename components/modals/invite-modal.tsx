5056f9d330085d18ffb8dea79cf1230b962c1a88
VK <24637852+krynv@users.noreply.github.com>
2024-01-02 10:30:34 +0000

    Styled the invite modal

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Copy, RefreshCw } from "lucide-react";
+
 import {
   Dialog,
   DialogContent,
@@ -7,6 +9,10 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { useModal } from "@/hooks/use-modal-store";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 
 export const InviteModal = () => {
   const { isOpen, onClose, type } = useModal();
@@ -21,7 +27,21 @@ export const InviteModal = () => {
             Invite
           </DialogTitle>
         </DialogHeader>
-
+        <div className="p-6">
+          <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+            Server invite link
+            <div className="flex items-center mt-2 gap-x-2">
+              <Input className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0" value="invite-link" />
+              <Button size="icon">
+                <Copy className="w-4 h-4" />
+              </Button>
+            </div>
+            <Button variant="link" size="sm" className="text-xs text-zinc-500 mt-4">
+              Generate a new link
+              <RefreshCw className="w-4 h-4 ml-2" />
+            </Button>
+          </Label>
+        </div>
       </DialogContent>
     </Dialog>
   );
