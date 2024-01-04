9478facaef19b3edfba68c30a3bd3cc9cc2ab00d
VK <24637852+krynv@users.noreply.github.com>
2024-01-04 17:34:37 +0000

    Added actions to server items in the server channel component

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -8,8 +8,10 @@ import {
 } from "@prisma/client";
 import { useParams, useRouter } from "next/navigation";
 
-import { Hash, Mic, Video } from "lucide-react";
 import { cn } from "@/lib/utils";
+
+import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
+import { ActionTooltip } from "@/components/action-tooltip";
 
 interface ServerChannelProps {
   channel: Channel;
@@ -51,6 +53,20 @@ export const ServerChannel = ({
       }>
         {channel.name}
       </p>
+      {channel.name.toLowerCase() !== "general" && role !== MemberRole.GUEST && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <ActionTooltip label="Edit">
+            <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-600 transition" />
+          </ActionTooltip>
+          <ActionTooltip label="Delete">
+            <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-600 transition" />
+          </ActionTooltip>
+        </div>
+      )}
+
+      {channel.name.toLowerCase() === "general" && (
+        <Lock className="hidden group-hover:block ml-auto w-4 h-4 text-zinc-600 dark:text-zinc-600" />
+      )}
 
     </button>
   )
