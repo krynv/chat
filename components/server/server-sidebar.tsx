bc9deef742e414a38bc275d929fd18ae1bd1d9b6
VK <24637852+krynv@users.noreply.github.com>
2024-01-04 16:56:09 +0000

    Added a component to handle rendering of channels in server

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,4 +1,9 @@
 import { redirect } from "next/navigation";
+
+import { ChannelType, MemberRole } from "@prisma/client";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
 import {
   Hash,
   Mic,
@@ -7,14 +12,13 @@ import {
   Video
 } from "lucide-react";
 
-import { ChannelType, MemberRole } from "@prisma/client";
-import { currentProfile } from "@/lib/current-profile";
-import { db } from "@/lib/db";
 
 import { ServerHeader } from "@/components/server/server-header";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { ServerSearch } from "@/components/server/server-search";
+import { ServerSection } from "@/components/server/server-section";
 
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -124,6 +128,13 @@ export const ServerSidebar = async ({
             }
           ]} />
         </div>
+        <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
+
+        {!!textChannels?.length && (
+          <div className="mb-2">
+            <ServerSection />
+          </div>
+        )}
       </ScrollArea>
     </div>
   );
