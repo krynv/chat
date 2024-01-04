58675e7f3dd3b8e35ebc077a92dba330a0d4c59b
VK <24637852+krynv@users.noreply.github.com>
2024-01-04 15:09:01 +0000

    Added a cmd/ctrl+k event listener

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Search } from "lucide-react";
 
 import {
@@ -29,6 +29,18 @@ export const ServerSearch = ({
 }: ServerSearchProps) => {
 
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    }
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
 
   return (
     <>
