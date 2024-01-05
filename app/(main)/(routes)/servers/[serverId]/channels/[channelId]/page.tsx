b1da577aee0ec5845b0b179bea261d2a17b16d0b
VK <24637852+krynv@users.noreply.github.com>
2024-01-05 14:46:20 +0000

    Added a chat header for when you click on a channel

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,53 @@
-const ChannelIdPage = () => {
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+import { ChatHeader } from "@/components/chat/chat-header";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+};
+
+const ChannelIdPage = async ({
+  params
+}: ChannelIdPageProps) => {
+
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const { serverId, channelId } = params;
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: channelId
+    }
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: serverId,
+      profileId: profile.id
+    }
+  });
+
+  if (!channel || !member) {
+    return redirect("/");
+  }
+
   return (
-    <div>
-      channel id page
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
     </div>
   );
 }
