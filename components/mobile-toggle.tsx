d9cafe9922f4bba03ad7f8ffe77f70db0b6e1ee9
VK <24637852+krynv@users.noreply.github.com>
2024-01-05 14:56:18 +0000

    Added the functionality to open the sidebar on mobile view

--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -1,8 +1,31 @@
 import { Menu } from "lucide-react";
 
-export const MobileToggle = () => {
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
+import { ServerSidebar } from "@/components/server/server-sidebar";
+
+
+export const MobileToggle = ({ serverId }: { serverId: string; }) => {
 
   return (
-    <Menu />
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 flex gap-0">
+        <div className="w-[72px]">
+          <NavigationSidebar />
+        </div>
+        <ServerSidebar serverId={serverId} />
+      </SheetContent>
+    </Sheet>
+
   );
 }
