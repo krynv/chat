1ac3fb9e9585dee74c2468d21cb651b3e5982a39
VK <24637852+krynv@users.noreply.github.com>
2024-01-05 19:10:29 +0000

    Updated to include the actual emoji picker from emoji-mart

--- components/emoji-picker.tsx
+++ components/emoji-picker.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { Smile } from "lucide-react";
+import Picker from "@emoji-mart/react";
+import data from "@emoji-mart/data";
+import { useTheme } from "next-themes";
 import {
   Popover,
   PopoverContent,
@@ -14,13 +17,15 @@ interface EmojiPickerProps {
 export const EmojiPicker = ({
   onChange
 }: EmojiPickerProps) => {
+  const { resolvedTheme } = useTheme();
+
   return (
     <Popover>
       <PopoverTrigger>
         <Smile className="text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition" />
       </PopoverTrigger>
-      <PopoverContent>
-
+      <PopoverContent side="right" sideOffset={40} className="bg-transparent border-none shadow-none drop-shadow-none mb-16">
+        <Picker data={data} onEmojiSelect={(emoji: any) => onChange(emoji.native)} theme={resolvedTheme} />
       </PopoverContent>
     </Popover>
   );
