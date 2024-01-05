91c1ffd00588647f367a9e3dc16fc48f76b85dfd
VK <24637852+krynv@users.noreply.github.com>
2024-01-05 10:39:24 +0000

    Fixed issue with default channel type when opening create channel modal

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -38,6 +38,7 @@ import { Button } from "@/components/ui/button";
 import { useParams, useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
 import { ChannelType } from "@prisma/client";
+import { useEffect } from "react";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -60,9 +61,18 @@ export const CreateChannelModal = () => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      type: ChannelType.TEXT,
+      type: channelType || ChannelType.TEXT,
     },
   });
+
+  useEffect(() => {
+    if (channelType) {
+      form.setValue("type", channelType);
+    }
+    else {
+      form.setValue("type", ChannelType.TEXT);
+    }
+  }, [channelType, form]);
 
   const isLoading = form.formState.isSubmitting;
 
@@ -133,7 +143,7 @@ export const CreateChannelModal = () => {
                     <Select
                       disabled={isLoading}
                       onValueChange={field.onChange}
-                      defaultValue={channelType || field.value}
+                      defaultValue={field.value}
                     >
                       <FormControl>
                         <SelectTrigger
