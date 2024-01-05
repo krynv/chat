d5fed750a0555349a5ac49602639e31634ea020c
VK <24637852+krynv@users.noreply.github.com>
2024-01-05 18:21:39 +0000

    Wrapped the form control under a form item

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -58,29 +58,31 @@ export const ChatInput = ({
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)}>
         <FormField control={form.control} name="content" render={({ field }) => (
-          <FormControl>
-            <div className="relative p-4 pb-6">
-              <button
-                type="button"
-                onClick={() => { }}
-                className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400
+          <FormItem>
+            <FormControl>
+              <div className="relative p-4 pb-6">
+                <button
+                  type="button"
+                  onClick={() => { }}
+                  className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400
                    hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex 
                     items-center justify-center">
-                <Plus className="text-white dark:text-[#313338]" />
-              </button>
-              <Input
-                disabled={isLoading}
-                className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none 
+                  <Plus className="text-white dark:text-[#313338]" />
+                </button>
+                <Input
+                  disabled={isLoading}
+                  className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none 
                   border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600
                 dark:text-zinc-200"
-                placeholder={`Message ${type === "conversation" ? name : `#${name}`}`}
-                {...field}
-              />
-              <div className="absolute top-7 right-8 ">
-                <Smile />
+                  placeholder={`Message ${type === "conversation" ? name : `#${name}`}`}
+                  {...field}
+                />
+                <div className="absolute top-7 right-8 ">
+                  <Smile />
+                </div>
               </div>
-            </div>
-          </FormControl>
+            </FormControl>
+          </FormItem>
         )} />
       </form>
     </Form>
