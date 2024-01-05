f8065baebdbf4cfe4e62fb797b81c8f49ebcaa68
VK <24637852+krynv@users.noreply.github.com>
2024-01-05 10:32:00 +0000

    Added styling around server member list. Now able to see server members underneath the channel list

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { Member, MemberRole, Profile, Server } from "@prisma/client";
-import { ShieldAlert, ShieldCheck } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
+
+import { UserAvatar } from "@/components/user-avatar";
 
 interface ServerMemberProps {
   member: Member & { profile: Profile };
@@ -26,8 +30,23 @@ export const ServerMember = ({
   const icon = roleIconMap[member.role];
 
   return (
-    <div>
-      Server Member
-    </div>
+    <button className={cn(
+      "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+      params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
+    )}>
+      <UserAvatar
+        src={member.profile.imageUrl}
+        className="h-8 w-8 md:h-8 md:w-8"
+      />
+      <p
+        className={cn(
+          "text-sm font-semibold text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
+          params?.channelId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+        )}
+      >
+        {member.profile.name}
+      </p>
+      {icon}
+    </button>
   )
 };
