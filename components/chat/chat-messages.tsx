023419423d6c8b1e833513dedc5dc2b48153a7ac
VK <24637852+krynv@users.noreply.github.com>
2024-01-06 02:39:15 +0000

    Added a loading indicator to the chat messages component

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { Member } from "@prisma/client";
+import { useChatQuery } from "@/hooks/use-chat-query";
 import { ChatWelcome } from "@/components/chat/chat-welcome";
+import { Loader2 } from "lucide-react";
 
 interface ChatMessagesProps {
   name: string;
@@ -26,6 +28,31 @@ export const ChatMessages = ({
   paramValue,
   type
 }: ChatMessagesProps) => {
+
+  const queryKey = `chat:${chatId}`;
+
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status
+  } = useChatQuery({
+    queryKey,
+    apiUrl,
+    paramKey,
+    paramValue
+  });
+
+  if (status === "pending") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+        <p className="text-sm text-zinc-500 dark:text-zinc-400">Loading messages...</p>
+      </div>
+    )
+  }
+
   return (
     <div className="flex-1 flex flex-col py-4 overflow-y-auto">
       <div className="flex-1" />
