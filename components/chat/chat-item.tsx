da6bf7e7d465a521a1e12f006ce53c0c0ba0066c
VK <24637852+krynv@users.noreply.github.com>
2024-01-06 13:48:41 +0000

    Styled the messages component to show who sent the message and when, along with message content

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { Member, Profile } from "@prisma/client";
+import { Member, MemberRole, Profile } from "@prisma/client";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
+
+import { UserAvatar } from "@/components/user-avatar";
+import { ActionTooltip } from "@/components/action-tooltip";
 
 interface ChatItemProps {
   id: string;
@@ -17,6 +21,13 @@ interface ChatItemProps {
   socketQuery: Record<string, string>;
 };
 
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+  [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />,
+};
+
+
 export const ChatItem = ({
   id,
   content,
@@ -30,8 +41,28 @@ export const ChatItem = ({
   socketQuery
 }: ChatItemProps) => {
   return (
-    <div>
-      Chat Item
+    <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
+      <div className="group flex gap-x-2 items-start w-full">
+        <div className="cursor-pointer hover:drop-shadow-md transition">
+          <UserAvatar src={member.profile.imageUrl} />
+        </div>
+        <div className="flex flex-col w-full">
+          <div className="flex items-center gap-x-2">
+            <div className="flex items-center">
+              <p className="font-semibold text-sm hover:underline cursor-pointer">
+                {member.profile.name}
+              </p>
+              <ActionTooltip label={member.role.toLocaleLowerCase()}>
+                {roleIconMap[member.role]}
+              </ActionTooltip>
+            </div>
+            <span className="text-xs text-zinc-500 dark:text-zinc-400">
+              {timestamp}
+            </span>
+          </div>
+          {content}
+        </div>
+      </div>
     </div>
   );
 };
