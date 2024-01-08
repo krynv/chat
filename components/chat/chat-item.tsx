a76c36cd105c9fa25617fb992bdfe1988b1905b2
VK <24637852+krynv@users.noreply.github.com>
2024-01-08 10:14:57 +0000

    Added an edit button to messages

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import { Member, MemberRole, Profile } from "@prisma/client";
-import { FileIcon, ShieldAlert, ShieldCheck } from "lucide-react";
+import { Edit, FileIcon, ShieldAlert, ShieldCheck } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
@@ -120,6 +120,19 @@ export const ChatItem = ({
           )}
         </div>
       </div>
-    </div >
+      {canDeleteMessage && (
+        <div className="hidden group-hover:flex items-center gap-x-2 
+          absolute p-1 top-2 right-5 bg-white dark:bg-zinc-800 border rounded-sm">
+          {canEditMessage && (
+            <ActionTooltip label="Edit">
+              <Edit
+                className="cursor-pointer ml-auto w-4 h-4 text-zinc-500
+                   hover:text-zinc-600 dark:hover:text-zinc-300 transition"
+              />
+            </ActionTooltip>
+          )}
+        </div>
+      )}
+    </div>
   );
 };
